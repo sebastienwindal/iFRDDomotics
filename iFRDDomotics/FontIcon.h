185f034ab6fd6747adc8d2cef8021e730e75acd2
Sebastien <sebastien@windal.net>
2013-05-16 00:37:07 -0400

    enable invalid SSL certs the right way, some cleanup, added iconn to the sensor list aswell...

---
+++ iFRDDomotics/FontIcon.h
@@ -0,0 +1,13 @@
+//
+//  FontIcon.h
+//  iFRDDomotics
+//
+//  Created by Sebastien on 5/15/13.
+//  Copyright (c) 2013 Sebastien. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FontIcon : NSObject
+
+@end
---
+++ iFRDDomotics/FontIcon.m
@@ -0,0 +1,13 @@
+//
+//  FontIcon.m
+//  iFRDDomotics
+//
+//  Created by Sebastien on 5/15/13.
+//  Copyright (c) 2013 Sebastien. All rights reserved.
+//
+
+#import "FontIcon.h"
+
+@implementation FontIcon
+
+@end
